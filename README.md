b768df53af674fb05da751ad75828aa83c7137f6
Quanmp <email.cua.ban@example.com>
2024-11-15 12:30:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# web-b-n-ph-ki-n-m-y-t-nh
---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleFilter() {
+    const overlay = document.getElementById('boLocOverlay');
+    overlay.style.display = overlay.style.display === 'flex' ? 'none' : 'flex';
+}
+
+function closeFilter() {
+    const overlay = document.getElementById('boLocOverlay');
+    overlay.style.display = 'none';
+}
+function toggleFilter1() {
+    const overlay = document.getElementById('boLocOverlay1');
+    overlay.style.display = overlay.style.display === 'flex' ? 'none' : 'flex';
+}
+function closeFilter1() {
+    const overlay = document.getElementById('boLocOverlay1');
+    overlay.style.display = 'none';
+}
+function toggleFilter2() {
+    const overlay = document.getElementById('boLocOverlay2');
+    overlay.style.display = overlay.style.display === 'flex' ? 'none' : 'flex';
+}
+function closeFilter2() {
+    const overlay = document.getElementById('boLocOverlay2');
+    overlay.style.display = 'none';
+}
